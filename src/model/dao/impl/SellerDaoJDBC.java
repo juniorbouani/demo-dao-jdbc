f6d741b6c07574fe7c2289212e95b525d2f5edd2
juniorbouani <juniorbouani@gmail.com>
2026-04-17 22:31:24 -0300

    delete increase

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -67,7 +67,6 @@ public class SellerDaoJDBC implements SellerDao {
             st = conn.prepareStatement("UPDATE seller "
                                     + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
                                     + "WHERE Id = ?");
-
             st.setString(1, obj.getName());
             st.setString(2, obj.getEmail());
             st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
@@ -88,6 +87,20 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+
+            st.setInt(1, id);
+            st.executeUpdate();
+
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatment(st);
+        }
 
     }
 
